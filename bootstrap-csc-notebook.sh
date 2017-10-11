abefb5079230dadc57c0219456ac1abed1628b30
Eetu Mäkelä <eetu.makela@iki.fi>
2017-10-11 15:56:56 +0300

    background R package installing in case that's causing current problems in bootup

--- bootstrap-csc-notebook.sh
+++ bootstrap-csc-notebook.sh
@@ -7,7 +7,7 @@ git clone https://github.com/jiemakel/dhintro.git
                                                                                                                                     
 mkdir -p ~/.jupyter/custom/                                                                                                         
 wget https://raw.githubusercontent.com/csc-training/python-introduction/gh-pages/assets/custom.js -O ~/.jupyter/custom/custom.js    
-Rscript -e 'install.packages(c("tm","SnowballC","wordcloud","slam"),repos="https://cloud.r-project.org/")'
+Rscript -e 'install.packages(c("tm","SnowballC","wordcloud","slam"),repos="https://cloud.r-project.org/")' &
 
 rm -f `basename "$0"`
 
