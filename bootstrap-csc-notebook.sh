94f8c5fb1a60032960843e33b34ff59fe42da2a5
Eetu Mäkelä <eetu.makela@iki.fi>
2017-10-18 18:26:22 +0300

    add scala version of intro

--- bootstrap-csc-notebook.sh
+++ bootstrap-csc-notebook.sh
@@ -8,6 +8,7 @@ git clone https://github.com/jiemakel/dhintro.git
 mkdir -p ~/.jupyter/custom/                                                                                                         
 wget https://raw.githubusercontent.com/csc-training/python-introduction/gh-pages/assets/custom.js -O ~/.jupyter/custom/custom.js    
 Rscript -e 'install.packages(c("tm","SnowballC","wordcloud","slam"),repos="https://cloud.r-project.org/")' &
+curl -sL https://raw.githubusercontent.com/alexarchambault/jupyter-scala/master/jupyter-scala | bash &
 
 rm -f `basename "$0"`
 
