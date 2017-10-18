6d0094a5aee25431467c9ff5e4a9acc7eeab4731
Eetu Mäkelä <eetu.makela@iki.fi>
2017-10-18 20:00:26 +0300

    fix path

--- bootstrap-csc-notebook.sh
+++ bootstrap-csc-notebook.sh
@@ -16,6 +16,6 @@ mkdir -p ~/.jupyter/custom/
 wget https://raw.githubusercontent.com/csc-training/python-introduction/gh-pages/assets/custom.js -O ~/.jupyter/custom/custom.js    
 Rscript -e 'install.packages(c("tm","SnowballC","wordcloud","slam"),repos="https://cloud.r-project.org/")' &
 ln -sf /home/jovyan/ /home/jovyan/?
-cd /home/jovyan && wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jre-8u152-linux-x64.tar.gz -O - | tar xz && export PATH=$PATH:/home/jovyan/jre1.8.0_152 && curl -sL https://raw.githubusercontent.com/alexarchambault/jupyter-scala/master/jupyter-scala | bash && cp /home/jovyan/work/dhintro/scala-kernel.json /home/jovyan/.local/share/jupyter/kernels/scala/kernel.json &
+cd /home/jovyan && wget -c --header "Cookie: oraclelicense=accept-securebackup-cookie" http://download.oracle.com/otn-pub/java/jdk/8u152-b16/aa0333dd3019491ca4f6ddbe78cdb6d0/jre-8u152-linux-x64.tar.gz -O - | tar xz && export PATH=$PATH:/home/jovyan/jre1.8.0_152/bin && curl -sL https://raw.githubusercontent.com/alexarchambault/jupyter-scala/master/jupyter-scala | bash && cp /home/jovyan/work/dhintro/scala-kernel.json /home/jovyan/.local/share/jupyter/kernels/scala/kernel.json &
 rm -f `basename "$0"`
 
