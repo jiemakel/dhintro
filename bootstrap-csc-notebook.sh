16a5b11aa2d21eb725f841fcc3070b586d9a42ae
Eetu Mäkelä <eetu.makela@iki.fi>
2017-10-18 18:45:43 +0300

    make sure environments update themselves

--- bootstrap-csc-notebook.sh
+++ bootstrap-csc-notebook.sh
@@ -1,10 +1,17 @@
 #!/bin/sh                                                                                                                           
                                                                                                                                     
 cd /home/jovyan/work                                                                                                                
-export GIT_COMMITTER_NAME=anonymous                                                                                                 
-export GIT_COMMITTER_EMAIL=anon@localhost                                                                                           
-git clone https://github.com/jiemakel/dhintro.git
-                                                                                                                                    
+git config --global user.email "anon@localhost" 
+git config --global user.name "anonymous"
+if [ ! -d dhintro/.git ]
+then
+    git clone https://github.com/jiemakel/dhintro.git
+else
+    cd dhintro
+    git stash
+    git pull https://github.com/jiemakel/dhintro.git
+    git checkout stash -- .
+fi
 mkdir -p ~/.jupyter/custom/                                                                                                         
 wget https://raw.githubusercontent.com/csc-training/python-introduction/gh-pages/assets/custom.js -O ~/.jupyter/custom/custom.js    
 Rscript -e 'install.packages(c("tm","SnowballC","wordcloud","slam"),repos="https://cloud.r-project.org/")' &
